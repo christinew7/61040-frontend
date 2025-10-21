f1db923502f2062a1d2ee66c1b6874a2b4c1b69f
Christine Wu <cwu27@mit.edu>
2025-10-21 14:58:16 -0400

    primary button component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    :type="type"
+    :disabled="disabled || loading"
+    class="primary-button"
+    @click="$emit('click')"
+    :aria-busy="loading ? 'true' : 'false'"
+  >
+    <span v-if="!loading"
+      ><slot>{{ label }}</slot></span
+    >
+    <span v-else class="spinner" aria-hidden="true">●●●</span>
+  </button>
+</template>
+
+<script setup>
+import "./PrimaryButton.css";
+
+// Props
+defineProps({
+  label: { type: String, default: "Primary" },
+  type: { type: String, default: "button" },
+  disabled: { type: Boolean, default: false },
+  loading: { type: Boolean, default: false },
+});
+
+// Emit events
+defineEmits(["click"]);
+</script>
