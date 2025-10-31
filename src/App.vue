bf12e12d8e5c83bd06b8df253b949138f1da9f60
Christine Wu <cwu27@mit.edu>
2025-10-30 21:38:13 -0400

    added new fonts

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,17 @@ onMounted(() => {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Fragment+Mono:ital@0;1&family=Signika:wght@300..700&display=swap");
+
 body {
+  font-family: "Fragment Mono", sans-serif;
   background-color: var(--color-bg-light);
   margin: 0;
   padding: 0;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Signika", Helvetica, Arial, sans-serif;
   text-align: center;
   background-color: var(--color-bg-light);
   min-height: 100vh;
