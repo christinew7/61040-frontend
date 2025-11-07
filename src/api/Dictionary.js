ebed0533233d7d0c50339c8a5fff23c9cd331302
Christine Wu <cwu27@mit.edu>
2025-11-07 02:23:57 -0500

    change api endpoint

--- src/api/Dictionary.js
+++ src/api/Dictionary.js
@@ -1,7 +1,7 @@
 // src/api/Dictionary.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
 
 // Create an Axios instance for the Dictionary concept
 const api = axios.create({
--- src/api/Library.js
+++ src/api/Library.js
@@ -1,7 +1,7 @@
 // src/api/Library.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
 const api = axios.create({
   baseURL: `${API_BASE_URL}/Library`,
   headers: { "Content-Type": "application/json" },
